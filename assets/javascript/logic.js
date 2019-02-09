e7794d80f6ac7754ce844be7fabeb72727720368
KrystalCW <krystal.wiederrich@gmail.com>
2019-02-08 22:28:47 -0800

    further progress

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -22,44 +22,79 @@ var hints = [
     "This story takes place in a world very similar to our own, but with some major twists."
 ]
 
-// var winsCounter = $("#wins-counter");
+var winsCounter = 0;
 // var word = $("#guessing-word");
 var selection = [];
 var dummyWord = [];
 var guessesRemaining = 10;
 var lettersGuessed = [];
+var gamePlay = true;
+// var positions = [];
 
 function computerSelect() {
-    var select = books[Math.floor(Math.random()*books.length)]
-    selection.push(select);
-    console.log(selection);
-    for (i = 0; i < select.length; i++) {
-        var dummyChar = select[i].replace(/[a-z]/, "_");
-        dummyWord += dummyChar;
+    if (gamePlay) {
+        var select = books[Math.floor(Math.random()*books.length)];
+        selection.push(select);
+        console.log(selection);
+        for (i = 0; i < select.length; i++) {
+            var dummyChar = select[i].replace(/[a-z]/, "_");
+            dummyWord += dummyChar;
+        }
+        // console.log(dummyWord);
+        display();
     }
-    console.log(dummyWord);
+}
+
+function display() {
+    $("#wins-counter").html(winsCounter);
     $("#guessing-word").html(dummyWord);
+    $("#guesses-counter").html(guessesRemaining);
+    $("#letters-guessed").html(lettersGuessed);
 }
 
 document.onkeydown = function userInput() {
+    gamePlay = false;
     if (event.keyCode >= 65 && event.keyCode <= 90) {
         var letter = event.key;
         console.log(letter);
-        for (i = 0; i < selection.length; i++) {
-            var position = selection[i].indexOf(letter);
-            // console.log(position);
-            if (position < 0) {
+        var positions = []
+        for (var i = 0; i < selection.length; i++) {
+            var indexNumber = selection[i].indexOf(letter);
+            if (indexNumber < 0) {
                 lettersGuessed.push(letter);
-                $("#letters-guessed").html(lettersGuessed);
+                // $("#letters-guessed").html(lettersGuessed);
                 guessesRemaining--;
-                $("#guesses-counter").html(guessesRemaining);
-            } 
-            else {
-                for (i = 0; i < position.length; i++) {
-                    dummyWord[position].replace("_", letter);
-                    console.log(dummyWord);
-                }
+                winLose();
+                // $("#guesses-counter").html(guessesRemaining);
+                display();
+            }
+            while (indexNumber > -1) {
+                positions.push(indexNumber);
+                indexNumber = selection[i].indexOf(letter, indexNumber +1); 
             }
         }
+        console.log(positions);
     }
-} 
+    for (var i = 0; i < positions.length; i++) {
+        dummyWord[positions[i]] = letter;
+        dummyWord.push;
+        display();
+    }
+    console.log(dummyWord);
+    // // $("#guessing-word").html(dummyWord);
+    // console.log(positions);
+}
+
+function winLose() {
+    if (dummyWord.indexOf("_") < 0) {
+        alert("Yay!! Great job!");
+        winsCounter++;
+    }
+    else if (guessesRemaining < 1) {
+        alert("Sorry! No more guesses. You lose :(");
+    }
+    gameplay = true;
+    computerSelect();
+    guessesRemaining = 10;
+    lettersGuessed = [];
+}
